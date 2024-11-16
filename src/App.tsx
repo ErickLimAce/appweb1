6fea92b21682313892b713ff126e80ce7a8b6e93
ErickLimAce <a01735902@tec.mx>
2024-11-16 03:25:25 -0600

    Pagina inicial

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,19 @@ import React from 'react';
 //import logo from './logo.svg';
 import './App.css';
 import Login from './components/Login';
+import Main from './components/Main';
 
-
+/**
+ * The main component of the application that renders the Login component.
+ */
 function App() {
   return (
     <div >
-      <Login />
+      <Main />
+    
     </div>
+    
+
   );
 }
 
---
+++ src/components/Main.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+export default function main() {
+  return (
+    <div>
+      <div className='bg-green-200'>
+        Main
+      </div>
+      <div className='bg-red-200'>
+        Division 1
+      </div>
+      <div className='bg-yellow-200'>
+        Division 2
+      </div>
+      <div className='bg-blue-200'>
+        Division 3
+      </div>
+
+    </div>
+  )
+}
